d7293d52e566b76f6ba4b659bcac3e79bc77b85a
webermarcolivier <webermarcolivier@gmail.com>
2018-11-09 12:25:38 +0100

    complete refactoring to match seaborn internals.

--- statannot.py
+++ statannot.py
@@ -3,7 +3,25 @@ import matplotlib.pyplot as plt
 from matplotlib import transforms, lines
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from seaborn.utils import remove_na
+
 from scipy import stats
+
+
+def stat_test(box_data1, box_data2, test):
+    testShortName = ''
+    formattedOutput = None
+    if test == 'Mann-Whitney':
+        u_stat, pval = stats.mannwhitneyu(box_data1, box_data2, alternative='two-sided')
+        testShortName = 'M.W.W.'
+        formattedOutput = "MWW RankSum two-sided P_val={:.3e} U_stat={:.3e}".format(pval, u_stat)
+    elif test == 't-test':
+        stat, pval = stats.ttest_ind(a=box_data1, b=box_data2)
+        testShortName = 't-test'
+        formattedOutput = "t-test independent samples, P_val={:.3e} stat={:.3e}".format(pval, stat)
+    
+    return pval, formattedOutput, testShortName
 
 
 
@@ -28,20 +46,72 @@ def pvalAnnotation_text(x, pvalueThresholds):
     return xAnnot if not singleValue else xAnnot.iloc[0]
 
 
-def add_statistical_test_annotation(
-        ax, df, catPairList, xlabel=None, ylabel=None, test='Mann-Whitney', order=None,
-        textFormat='star', loc='inside',
-        pvalueThresholds=[[1e9,"ns"], [0.05,"*"], [1e-2,"**"], [1e-3,"***"], [1e-4,"****"]],
-        color='0.2', lineYOffsetAxesCoord=None, lineHeightAxesCoord=0.02, yTextOffsetPoints=1,
-        linewidth=1.5, fontsize='medium', verbose=1):
-
+def add_stat_annotation(ax,
+                        data=None, x=None, y=None, hue=None, order=None, hue_order=None,
+                        boxPairList=None,
+                        test='Mann-Whitney', textFormat='star', loc='inside',
+                        pvalueThresholds=[[1e9,"ns"], [0.05,"*"], [1e-2,"**"], [1e-3,"***"], [1e-4,"****"]],
+                        color='0.2', lineYOffsetAxesCoord=None, lineHeightAxesCoord=0.02, yTextOffsetPoints=1,
+                        linewidth=1.5, fontsize='medium', verbose=1):
     """
-    """
+    User should use the same argument for the data, x, y, hue, order, hue_order as the seaborn boxplot function.
     
-    if loc not in ['inside', 'outside']:
-        raise ValueError("loc value should be inside or outside.")
-    if test not in ['t-test', 'Mann-Whitney']:
-        raise ValueError("test name incorrect.")
+    boxPairList can be of either form:
+    For non-grouped boxplot: [(cat1, cat2), (cat3, cat4)]
+    For boxplot grouped by hue: [((cat1, hue1), (cat2, hue2)), ((cat3, hue3), (cat4, hue4))]
+    """
+
+    def find_x_position_box(boxPlotter, boxName):
+        """
+        boxName can be either a name "cat" or a tuple ("cat", "hue")
+        """
+        if boxPlotter.plot_hues is None:
+            cat = boxName
+            hueOffset = 0
+        else:
+            cat = boxName[0]
+            hue = boxName[1]
+            hueOffset = boxPlotter.hue_offsets[boxPlotter.hue_names.index(hue)]
+
+        groupPos = boxPlotter.group_names.index(cat)
+        boxPos = groupPos + hueOffset
+        return boxPos
+
+    
+    def get_box_data(boxPlotter, boxName):
+        """
+        boxName can be either a name "cat" or a tuple ("cat", "hue")
+        
+        Here we really have to duplicate seaborn code, because there is not direct access to the
+        box_data in the BoxPlotter class.
+        """
+        if boxPlotter.plot_hues is None:
+            cat = boxName
+        else:
+            cat = boxName[0]
+            hue = boxName[1]
+
+        i = boxPlotter.group_names.index(cat)
+        group_data = boxPlotter.plot_data[i]
+
+        if boxPlotter.plot_hues is None:
+            # Draw a single box or a set of boxes
+            # with a single level of grouping
+            box_data = remove_na(group_data)
+        else:
+            hue_level = hue
+            hue_mask = boxPlotter.plot_hues[i] == hue_level
+            box_data = remove_na(group_data[hue_mask])
+
+        return box_data
+    
+    
+    validList = ['inside', 'outside']
+    if loc not in validList:
+        raise ValueError("loc value should be one of the following: {}.".format(', '.join(validList)))
+    validList = ['t-test', 'Mann-Whitney']
+    if test not in validList:
+        raise ValueError("test value should be one of the following: {}.".format(', '.join(validList)))
 
     if verbose >= 1 and textFormat == 'star':
         print("pvalue annotation legend:")
@@ -53,23 +123,13 @@ def add_statistical_test_annotation(
                 print('{}: p <= {:.2e}'.format(pvalueThresholds[i][1], pvalueThresholds[i][0]))
         print()
 
-    yStack = []
-    
-    if xlabel is None:
-        # Guess the xlabel based on the xaxis label text
-        xlabel = ax.xaxis.get_label().get_text()
-    if ylabel is None:
-        # Guess the xlabel based on the xaxis label text
-        ylabel = ax.yaxis.get_label().get_text()
+    # Create the same BoxPlotter object as seaborn's boxplot
+    boxPlotter = sns.categorical._BoxPlotter(x, y, hue, data, order, hue_order,
+                                             orient=None, width=.8, color=None, palette=None, saturation=.75,
+                                             dodge=True, fliersize=5, linewidth=None)
+    plotData = boxPlotter.plot_data
     
     xtickslabels = [t.get_text() for t in ax.xaxis.get_ticklabels()]
-    
-    g = df.groupby(xlabel)
-    catValues = df[xlabel].unique()
-    catValues = np.array(xtickslabels)
-    if order is not None:
-        catValues = order
-
     ylim = ax.get_ylim()
     yRange = ylim[1] - ylim[0]
     if loc == 'inside':
@@ -78,38 +138,52 @@ def add_statistical_test_annotation(
         lineYOffsetAxesCoord = 0.03
     yOffset = lineYOffsetAxesCoord*yRange
 
+    yStack = []
     annList = []
-    for cat1, cat2 in catPairList:
-        if cat1 in catValues and cat2 in catValues:
-            # Get position of bars 1 and 2
-            x1 = np.where(catValues == cat1)[0][0]
-            x2 = np.where(catValues == cat2)[0][0]
+    for box1, box2 in boxPairList:
+        
+        valid = None
+        groupNames = boxPlotter.group_names
+        hueNames = boxPlotter.hue_names
+        if boxPlotter.plot_hues is None:
+            cat1 = box1
+            cat2 = box2
+            hue1 = None
+            hue2 = None
+            label1 = '{}'.format(cat1)
+            label2 = '{}'.format(cat2)
+            valid = cat1 in groupNames and cat2 in groupNames
+        else:
+            cat1 = box1[0]
+            hue1 = box1[1]
+            cat2 = box2[0]
+            hue2 = box2[1]
+            label1 = '{}_{}'.format(cat1, hue1)
+            label2 = '{}_{}'.format(cat2, hue2)
+            valid = cat1 in groupNames and cat2 in groupNames and hue1 in hueNames and hue2 in hueNames
+        
+        
+        if valid:
+            # Get position of boxes
+            x1 = find_x_position_box(boxPlotter, box1)
+            x2 = find_x_position_box(boxPlotter, box2)
+            box_data1 = get_box_data(boxPlotter, box1)
+            box_data2 = get_box_data(boxPlotter, box2)
+            ymax1 = box_data1.max()
+            ymax2 = box_data2.max()
 
-            cat1YMax = g[ylabel].max().loc[cat1]
-            cat2YMax = g[ylabel].max().loc[cat2]
-
-            cat1Values = g.get_group(cat1)[ylabel].values
-            cat2Values = g.get_group(cat2)[ylabel].values
-
-            testShortName = ''
-            if test == 'Mann-Whitney':
-                u_stat, pval = stats.mannwhitneyu(cat1Values, cat2Values, alternative='two-sided')
-                testShortName = 'M.W.W.'
-                if verbose >= 2: print ("{} v.s. {}: MWW RankSum P_val={:.3e} U_stat={:.3e}".format(cat1, cat2, pval, u_stat))
-            elif test == 't-test':
-                stat, pval = stats.ttest_ind(a=cat1Values, b=cat2Values)
-                testShortName = 't-test'
-                if verbose >= 2: print ("{} v.s. {}: t-test independent samples, P_val=={:.3e} stat={:.3e}".format(cat1, cat2, pval, stat))
+            pval, formattedOutput, testShortName = stat_test(box_data1, box_data2, test)
+            if verbose >= 2: print ("{} v.s. {}: {}".format(label1, label2, formattedOutput))
 
             if textFormat == 'full':
                 text = "{} p < {:.2e}".format(testShortName, pval)
             elif textFormat is None:
-                text = ''
+                text = None
             elif textFormat is 'star':
                 text = pvalAnnotation_text(pval, pvalueThresholds)
             
             if loc == 'inside':
-                yRef = max(cat1YMax, cat2YMax)
+                yRef = max(ymax1, ymax2)
             elif loc == 'outside':
                 yRef = ylim[1]
 
@@ -128,17 +202,27 @@ def add_statistical_test_annotation(
                 line.set_clip_on(False)
                 ax.add_line(line)
             
-            ann = ax.annotate(text, xy=(np.mean([x1, x2]), y + h),
-                              xytext=(0, yTextOffsetPoints), textcoords='offset points',
-                              xycoords='data', ha='center', va='bottom', fontsize=fontsize,
-                              clip_on=False, annotation_clip=False)
-            annList.append(ann)
+            if text is not None:
+                ann = ax.annotate(text, xy=(np.mean([x1, x2]), y + h),
+                                  xytext=(0, yTextOffsetPoints), textcoords='offset points',
+                                  xycoords='data', ha='center', va='bottom', fontsize=fontsize,
+                                  clip_on=False, annotation_clip=False)
+                annList.append(ann)
+                
             ax.set_ylim((ylim[0], 1.1*(y + h)))
-            plt.draw()
-            bbox = ann.get_window_extent()
-            bbox_data = bbox.transformed(ax.transData.inverted())
-            yTopAnnot = bbox_data.ymax
+            
+            if text is not None:
+                plt.draw()
+                bbox = ann.get_window_extent()
+                bbox_data = bbox.transformed(ax.transData.inverted())
+                yTopAnnot = bbox_data.ymax
+            else:
+                yTopAnnot = y + h
+
             yStack.append(yTopAnnot)
+        else:
+            raise ValueError("boxPairList contains an unvalid box pair.")
+            pass
             
         
     yStackMax = max(yStack)
