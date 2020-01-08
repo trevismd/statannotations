13f471c919046fee0214690e88c9b173fb1ab037
Marc Weber <webermarcolivier@gmail.com>
2020-01-08 14:54:12 +0100

    doc: v0.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="statannot",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=open("requirements.txt").readlines(),
     python_requires='>=3.5',
