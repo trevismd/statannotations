1dd7714f64fc39f26a7df787366c4449f5a875b0
getzze <getzze@gmail.com>
2024-07-25 13:02:56 +0100

    compat with py37, remove Literal import

--- statannotations/_Plotter.py
+++ statannotations/_Plotter.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING
 
 import numpy as np
 import matplotlib.pyplot as plt
